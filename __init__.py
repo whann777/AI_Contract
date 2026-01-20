44c65614a734c8bb640a933bc8efeea16c8d38a4
whann777 <panjai.ben@gmail.com>
2026-01-20 23:03:08 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Core business logic modules - Preserved from AI_Contract_V2.ipynb"""
+
+from .ai_analyzer import TTADocumentAnalyzer
+from .data_processor import DataPreprocessor
+from .reconciliation import TTAReconciliationSystem
+
+__all__ = ['TTADocumentAnalyzer', 'DataPreprocessor', 'TTAReconciliationSystem']
