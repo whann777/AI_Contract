49e6c546139778d6c84dfb209ef7a4d990b76ded
whann777 <panjai.ben@gmail.com>
2026-01-21 00:27:13 +0700

    Fix formatting of __all__ in __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -5,7 +5,7 @@ from .analyze_mode import show_analyze_mode
 from .auditor_mode import show_auditor_mode
 
 __all__ = [
-    'show_landing_page', 
+    'show_landing_page',
     'show_info_sidebar',
     'show_analyze_mode',
     'show_auditor_mode'
